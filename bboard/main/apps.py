44722dbcd52b5198e5a76b34d85d263aa2c34d33
VectonZed <mp.matv@gmail.com>
2023-05-11 20:21:30 +0500

    first commit

---
+++ bboard/main/apps.py
@@ -0,0 +1,8 @@
+#  coding: cp1251
+from django.apps import AppConfig
+
+
+class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'main'
+    verbose_name = "Доска объявлений"
