4cc2f8673ff9984fe28072b8b1982d2cf5d558c2
Vecton <133243188+VectonZed@users.noreply.github.com>
2023-05-13 00:50:39 +0500

    Add files via upload

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, profile, BBLogoutView, BBPasswordChangeView, ChangeUserInfoView, RegisterUserView, RegisterDoneView, user_activate
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, BBPasswordChangeView, ChangeUserInfoView, RegisterUserView, RegisterDoneView, user_activate, DeleteUserView
 
 app_name = 'main'
 
 urlpatterns = [
+	path('accounts/profile/delete', DeleteUserView.as_view(), name='profile_delete'),
 	path('accounts/profile/change', ChangeUserInfoView.as_view(), name='profile_change'),
 	path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
 	path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
 	path('accounts/profile/', profile, name='profile'),
 	path('accounts/register/activate/<str:sign>/', user_activate, name='register_activate'),
-	path('accounts/register/done/', RegisterDoneView.as_view(), name='register done'),
+	path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
 	path('accounts/register/', RegisterUserView.as_view(), name='register'),
 	path('accounts/login/', BBLoginView.as_view(), name='login'),
 	path('<str:page>/', other_page, name='other'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -5,12 +5,14 @@ from django.template.loader import get_template
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
 from django.core.signing import BadSignature
+from django.contrib.auth import logout
+from django.contrib import messages
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm, RegisterUserForm
@@ -83,3 +85,22 @@ def user_activate(request, sign):
 		user.is_activated = True
 		user.save()
 	return render(request. template)
+
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+	model = AdvUser
+	template_name = 'main/delete_user.html'
+	success_url = reverse_lazy('main:index')
+
+	def setup(self, request, *args, **kwargs):
+		self.user_id = request.user.pk
+		return super().setup(request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		logout(request)
+		messages.add_message(request, messages.SUCCESS, 'Пользователь удален')
+		return super().post(request, *args, **kwargs)
+
+	def get_object(self, queryset=None):
+		if not queryset:
+			queryset = self.get_queryset()
+		return get_object_or_404(queryset, pk=self.user_id)
